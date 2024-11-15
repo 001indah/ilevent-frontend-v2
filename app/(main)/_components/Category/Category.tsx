8c9de98735be7f713eb7b6652fb2401947f0ce5e
001indah <163018968+001indah@users.noreply.github.com>
2024-11-15 11:21:45 +0700

    Update Category.tsx

--- app/(main)/_components/Category/Category.tsx
+++ app/(main)/_components/Category/Category.tsx
@@ -1,20 +1,3 @@
-
-
-
-// const [categories, setCategories] = useState([]);
-
-// useEffect(() => {
-//     fetchCategories();
-// }, []);
-
-// const fetchCategories = async () => {
-//     try {
-//         const response = await axios.get('/api/categories'); // Sesuaikan dengan URL endpoint yang sesuai
-//         setCategories(response.data);
-//     } catch (error) {
-//         console.error('Error fetching categories:', error);
-//     }
-// };
 import React from 'react'
 import CategoryCard from './CategoryCard'
 
@@ -32,7 +15,6 @@ const Category: React.FC = () => {
         { id: 1, title: 'Exhibition', eventCount: 12, imageUrl: 'https://images.pexels.com/photos/69903/pexels-photo-69903.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
         { id: 2, title: 'Workshop', eventCount: 8, imageUrl: 'https://images.pexels.com/photos/1709003/pexels-photo-1709003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
         { id: 3, title: 'Seminar', eventCount: 5, imageUrl: 'https://images.unsplash.com/photo-1587825140708-dfaf72ae4b04?fm=jpg&w=3000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-        // ... other categories ...
     ];
 
     return (
